664208d540539c17d91d278491a83c6179654291
ggleroy <112895265+ggleroy@users.noreply.github.com>
2025-02-23 20:51:17 -0300

    Adding a channel to wait group example

--- funstuff/wait_group.go
+++ funstuff/wait_group.go
@@ -5,18 +5,35 @@ import (
 	"sync"
 )
 
-func reader(id int, wg *sync.WaitGroup) {
+func reader(id int, ch <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Printf("Reader Leroy %d\n", id)
+	for {
+		val, ok := <-ch
+		if !ok {
+			fmt.Println("Channel Closed")
+			return
+		}
+		fmt.Printf("Reader %d Received %d\n", id, val)
+	}
 }
 
 func main() {
 
 	var wg sync.WaitGroup
 
-	wg.Add(2)
-	go reader(1, &wg)
-	go reader(2, &wg)
+	ch := make(chan int)
+
+	wg.Add(4)
+
+	go reader(1, ch, &wg)
+	go reader(2, ch, &wg)
+	go reader(3, ch, &wg)
+	go reader(4, ch, &wg)
+
+	for i := 0; i < 100; i++ {
+		ch <- i
+	}
+	close(ch)
 
 	wg.Wait()
 }
